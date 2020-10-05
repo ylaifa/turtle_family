5fbe7e77b4f41e8ffcd62539ea2b18ce4ac1efef
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-05 13:41:08 +0200

    add test to turtle_spec

--- spec/models/turtle_spec.rb
+++ spec/models/turtle_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Turtle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is creatable" do 
+    turtle = create(:turtle)
+    first_turtle = Turtle.first
+    expect(first_turtle.name).to eq(turtle.name)
+    expect(first_turtle.color).to eq(turtle.color)
+  end
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:color) }
 end
