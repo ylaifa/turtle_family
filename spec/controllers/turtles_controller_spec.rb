abb79f0c01e03c71d4c225a89e1f722178cb465d
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-06 10:38:03 +0200

    edit turtles_controller

---
+++ spec/controllers/turtles_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe TurtlesController, type: :controller do
+    describe "#index"
+    describe "#show"
+    describe "#create"
+    describe "#update"
+    describe "#destroy" 
+end
