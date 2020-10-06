9131b5ab2da412a020af9286227209f71c19ec34
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-06 12:27:45 +0200

    add test to show method

--- spec/controllers/turtles_controller_spec.rb
+++ spec/controllers/turtles_controller_spec.rb
@@ -2,7 +2,21 @@ require 'rails_helper'
 
 RSpec.describe TurtlesController, type: :controller do
     describe "#index"
-    describe "#show"
+    describe "#show" do
+        let(:turtle) { create(:turtle) }
+        let(:id) { turtle.id }
+
+        subject do
+            get :show, params: { id: id }
+        end
+
+        it "returns the turtle" do
+            subject
+            expect(json_response[:turtle][:id]).to eq(turtle.id)
+            expect(json_response[:turtle][:name]).to eq(turtle.name)
+            expect(json_response[:turtle][:color]).to eq(turtle.color)
+        end 
+    end
     describe "#create"
     describe "#update"
     describe "#destroy" 
