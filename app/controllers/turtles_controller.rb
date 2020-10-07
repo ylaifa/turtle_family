d696e7d286660c863eff687735303d8136fe7caa
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-07 17:01:32 +0200

    edit turtles_controller add create and update method

--- app/controllers/turtles_controller.rb
+++ app/controllers/turtles_controller.rb
@@ -11,13 +11,38 @@ class TurtlesController < ApplicationController
     render json: turtle
   end
 
-  def create; end
+	def create 
+		turtle = Turtle.create(create_params)
+		if turtle.errors.empty?
+			render json: turtle
+		else
+			render json: { errors: turtle.errors }, status: :forbidden
+		end
+	end
 
-  def update; end
+	def update
+		turtle = Turtle.find(params[:id])
+		turtle.update(update_params)
+		if turtle.errors.empty?
+			render json: turtle
+		else
+			render json: { errors: turtle.errors }, status: :forbidden
+		end
+	end
 
   def destroy
     turtle = Turtle.find(params[:id])
     turtle.destroy
     head :no_content
-  end
+	end
+	
+	private
+
+	def create_params
+		params.permit(:name, :color)
+	end
+
+	def update_params
+		params.permit(:name, :color)
+	end
 end
