91621f18b277781ec2eb8cbd47209e59f19eda9e
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-29 15:49:02 +0100

    add after_create turtle send_email_to_creator

--- app/models/turtle.rb
+++ app/models/turtle.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Turtle < ApplicationRecord
+  after_create :send_email_to_creator, if: -> { email }
+  
   validates :name, presence: true
   validates :color, presence: true
+
+  def send_email_to_creator
+    TurtleMailer.create(self).deliver_now
+  end
 end
