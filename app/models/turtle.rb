56564f944f4e366352dc68cb9ca693545f728a17
Yousri LAÏFA <laifayousri@gmail.com>
2020-10-30 16:33:30 +0100

    edit Turtle model change deliver_now to deliver_later

--- app/models/turtle.rb
+++ app/models/turtle.rb
@@ -2,11 +2,11 @@
 
 class Turtle < ApplicationRecord
   after_create :send_email_to_creator, if: -> { email }
-  
+
   validates :name, presence: true
   validates :color, presence: true
 
   def send_email_to_creator
-    TurtleMailer.create(self).deliver_now
+    TurtleMailer.create(self).deliver_later
   end
 end
